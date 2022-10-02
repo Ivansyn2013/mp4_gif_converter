d620597645076ceb846af1445bf97cbd79dbfeab
ivan <ivansyn2013@yandex.ru>
2022-10-02 14:47:35 +0300

    mp4 in jpeg and gif_maker work. GUI not adapted

--- gif_maker.py
+++ gif_maker.py
@@ -5,12 +5,12 @@ def make_gif(frame_folder):
     images = glob.glob(f"{frame_folder}/*.jpg")
     images.sort()
     frames = [Image.open(image) for image in images]
+    frames = [frame.crop((100,0,725,625)) for frame in frames] # Для обрезки
     frame_one = frames[0]
-    frame_one.save("test.gif", format="GIF", append_image=frames,
-                   save_all=True, duration=50, loop=0)
+    frame_one.save("test.gif",  save_all=True, append_images=frames[1:], optimize=True, duration=200,
+                   loop=0) # сборка всех фреймов в фаил и другие настройки
 
 
 if __name__ == '__main__':
     make_gif("outout")
 
-make_gif('outout')
